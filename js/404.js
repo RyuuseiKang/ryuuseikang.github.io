46c55aefe4847480cb93ef5d556f67b478c6d106
RyuuseiKang <ryuuseikang@naver.com>
2019-01-18 06:28:07 +0900

    Add files via upload

--- js/404.js
+++ js/404.js
@@ -1,3 +1,13 @@
+var ImageName = ["IMG_0101",
+                 "IMG_0108",
+                 "IMG_1594",
+                 "IMG_9551"];
+
 function GoToHomePage(){
     window.location = '/';   
 }
+
+window.onload = function(){
+    document.body.style.background = "url('img/" + ImageName[Math.floor(Math.random() * 4)] + ".JPG') no-repeat center center fixed";
+    document.body.style.backgroundSize = "cover";
+};
