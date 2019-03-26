26646e50055715b8cdd51a7998bde51b26947f01
RyuuseiKang <ryuuseikang@naver.com>
2019-03-26 22:50:13 +0900

    졸리당

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-(function() {
+/*
+function() {
     var header = $(".navbar");
 
     $(window).scroll(function() {
@@ -7,18 +8,49 @@
         switch(scroll / 80) {
             case 0:
                 header.addClass("scrolled");
+                alert('0');
                 break;
             case 1:
+                alert('1');
+                break;
+            case 2:
+                alert('2');
                 break;
             default:
                 break;
         }
 
-        if (scroll >= 80) {
+        if (scroll >= 20) {
             header.addClass("scrolled");
         } else {
             header.removeClass("scrolled");
         }
     });
+}
 
+*/
+window.addEventListener('scroll', function() {
+    var el = document.querySelector('.show-on-scroll');
+
+    console.log(window.scrollY / 100);
+
+    /*
+    switch(window.scrollY / 120) {
+        case 0:
+            //header.addClass("scrolled");
+            alert('0');
+            break;
+        case 1:
+            alert('1');
+            break;
+        case 2:
+            alert('2');
+            break;
+        default:
+            break;
+    }
+
+    if(window.scrollY >= 400) el.classList.add('shown');
+    else el.classList.remove('shown');
+    */
 });
