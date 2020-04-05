9bb18934cc93d1eba3a3794722951761b571e744
RyuuseiKang <ryuuseikang@naver.com>
2020-04-05 19:58:32 +0900

    주석 삭제

--- ColorPicker2/js/ColorPicker2.js
+++ ColorPicker2/js/ColorPicker2.js
@@ -1,5 +1,3 @@
 function Download(){
-    //window.open('/ryuuseikang.github.io/ColorPicker2/download.html');
-    //window.open('/ryuuseikang.github.io/ColorPicker2/download.html', 'newWindow');
     window.location.assign('/ryuuseikang.github.io/ColorPicker2/download.html');
 }
